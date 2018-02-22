bb2cad42af6ec9a3e60c61f15f69e4ee64ded7ba
Eric Bredder <ebredder@users.noreply.github.com>
2018-02-22 01:09:28 -0500

    Update TIP120_Motor_Driver.ino

--- Arduino/TIP120_Motor_Driver.ino
+++ Arduino/TIP120_Motor_Driver.ino
@@ -1,14 +1,11 @@
-int potPin = 0; // Analog in 0 connected to the potentiometer
+int potPin = A0; // Analog input 0 connected to the potentiometer
 int transistorPin = 9; // PWM Pin 9 connected to the base of the transistor
 int potValue = 0; // value returned from the potentiometer
 
 void setup() {
-  // set the transistor pin as output:
-  pinMode(transistorPin, OUTPUT);
+  pinMode(transistorPin, OUTPUT); // set the transistor pin as output:
 }
 void loop() {
-  // read the potentiometer, convert it to 0 - 255:
-  potValue = analogRead(potPin) / 4;
-  // use that to control the transistor:
-  analogWrite(transistorPin, potValue);
+  potValue = analogRead(potPin) / 4; // read the potentiometer, convert it to 0 - 255:
+  analogWrite(transistorPin, potValue); // use that to control the transistor:
 }
